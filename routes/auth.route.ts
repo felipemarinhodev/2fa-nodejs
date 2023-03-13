6b30bd3a2e60d0764639526ea24f16cc78764453
felipemarinhodev <felipmarinho@gmail.com>
2023-03-13 11:51:49 -0700

    commit

---
+++ routes/auth.route.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import authController from "../controllers/auth.controller";
+
+export const router = express.Router();
+
+router.post('/register', authController.RegisterUser)
+router.post('/login', authController.LoginUser)
+router.post('/otp/generate', authController.GenerateOTP)
+router.post('/otp/verify', authController.VerifyOTP)
+router.post('/otp/validate', authController.ValidateOTP)
+router.post('/otp/disable', authController.DisableOTP)
+
+
+export default router
---
+++ server.ts
@@ -0,0 +1,55 @@
+import { PrismaClient } from "@prisma/client";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import authRouter from "./routes/auth.route";
+
+export const prisma = new PrismaClient();
+const app = express();
+
+async function main() {
+  // Middleware
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    })
+  );
+  app.use(express.json());
+
+  //   Health Checker
+  app.get("/api/healthchecker", (req: Request, res: Response) => {
+    res.status(200).json({
+      status: "success",
+      message: "Welcome to Two-Factor Authentication with Node.js",
+    });
+  });
+
+  app.use("/api/auth", authRouter)
+
+  // Register the API Routes
+
+  // Catch All
+  app.all("*", (req: Request, res: Response) => {
+    return res.status(404).json({
+      status: "fail",
+      message: `Route: ${req.originalUrl} not found`,
+    });
+  });
+
+  const PORT = 8000;
+  app.listen(PORT, () => {
+    console.info(`Server started on port: ${PORT}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
